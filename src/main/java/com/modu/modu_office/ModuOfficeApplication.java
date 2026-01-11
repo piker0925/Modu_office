7aaef7207ea0c9e6efcf9fe915f27ed58a139193
JunSeo Oh <piker0925@gmail.com>
2026-01-11 15:06:26 +0900

    기본 파일 환경 추가

---
+++ src/main/java/com/modu/modu_office/ModuOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.modu.modu_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modu/modu_office/ModuOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modu.modu_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
